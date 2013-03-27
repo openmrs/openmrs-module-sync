d3f337b24f8c5819adb021cfea08eda469b4ef5e
Michael Seaton <mseaton@pih.org>
2013-03-27 13:38:35 -0400

    Removing @Ignore on unit test that was fixed in SYNC-256

--- api/src/test/java/org/openmrs/module/sync/SyncUserTest.java
+++ api/src/test/java/org/openmrs/module/sync/SyncUserTest.java
@@ -343,8 +343,6 @@ public class SyncUserTest extends SyncBaseTest {
 
 	@Test
 	@NotTransactional
-    // still need to determine why this test is failing: SYNC-256
-    @Ignore
 	public void shouldSyncUserPropertyRemove() throws Exception {
 		runSyncTest(new SyncTestHelper() {
 			
