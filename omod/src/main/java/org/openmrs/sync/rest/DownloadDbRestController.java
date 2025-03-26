934b35cede1ebd92bdbc1ae999c43dcf6ade737c
Cosmin <cioan@pih.org>
2025-03-26 12:01:45 -0400

    SYNC-364: refactor response type

--- omod/src/main/java/org/openmrs/sync/rest/DownloadDbRestController.java
+++ omod/src/main/java/org/openmrs/sync/rest/DownloadDbRestController.java
@@ -10,7 +10,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.*;
@@ -22,16 +21,16 @@ public class DownloadDbRestController {
     private static String MANAGE_SYNC_PRIVILEGE = "Manage Synchronization";
 
     @GetMapping(value = "/rest/v1/sync/downloadDb")
-    public ResponseEntity<InputStreamResource> getDbBackup() throws IOException {
+    public ResponseEntity<byte[]> getDbBackup() throws IOException {
         if (Context.hasPrivilege(MANAGE_SYNC_PRIVILEGE)) {
             try {
                 File outputFile = Context.getService(SyncService.class).generateDataFile();
                 InputStream in = new FileInputStream(outputFile);
                 MediaType mediaType = MediaType.parseMediaType("application/octet-stream");
-                return ResponseEntity.ok().contentType(mediaType).body(new InputStreamResource(in));
+                return ResponseEntity.ok().contentType(mediaType).body(IOUtils.toByteArray(in));
             } catch (Exception e) {
                 log.warn("Error while generating backup file", e);
-                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new InputStreamResource(new ByteArrayInputStream(e.getMessage().getBytes())));
+                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage().getBytes());
             }
         } else {
             return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
